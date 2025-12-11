133812591436a54b01a1e7d7d377e145ebddc0a2
友輝 松島 <oibc36061@ojs.ac.jp>
2025-12-11 15:57:30 +0900

    ページルーティングがうまく機能しないバグの修正

---
+++ StoryDesignSupportWebApp/StoryDesignSupportWebApp/Data/Project.cs
@@ -0,0 +1,32 @@
+﻿namespace StoryDesignSupportWebApp.Data {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Project {
+        public const int MaxGenreSize = 5;
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = "";
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = "";
+
+        [MaxLength(50)]
+        public string[]? Genre { get; set; }
+
+        [MaxLength(400)]
+        public string Overview { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public Project() {
+            //初期化
+            Genre = new string[MaxGenreSize];
+        }
+    }
+}
