e42beabdb7b86c01b331e234a98e6959f171245a
友輝 松島 <oibc36061@ojs.ac.jp>
2026-02-24 11:07:27 +0900

    UI改善テスト

--- StoryDesignSupportWebApp/StoryDesignSupportWebApp/Data/EditData/MaterialItem.cs
+++ StoryDesignSupportWebApp/StoryDesignSupportWebApp/Data/EditData/MaterialItem.cs
@@ -1,6 +1,7 @@
 ﻿namespace StoryDesignSupportWebApp.Data.EditData {
     public class MaterialItem {
         public string Title { get; set; } = "";
+        public string[]? Label { get; set; }
         public string Text { get; set; } = "";
     }
 }
